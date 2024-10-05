56d84a46b03e71dcffdcefdc4f273d58c5b74fac
Jakub Porzuczek <=>
2024-10-05 18:29:48 +0200

    Initial commit

---
+++ src/main/java/com/monodev/ummaker/UnmatchedCardMakerApplication.java
@@ -0,0 +1,13 @@
+package com.monodev.ummaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnmatchedCardMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnmatchedCardMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monodev/ummaker/UnmatchedCardMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monodev.ummaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnmatchedCardMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
